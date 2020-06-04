7b097bc4d3246981ca2e3c16fb5f277dc542b201
oscar <ol00130@surrey.ac.uk>
2020-06-04 14:17:50 +0200

    "SRS commented"

--- srs.ex
+++ srs.ex
@@ -1,38 +1,43 @@
 defmodule SRS do
-  def reserve(pid, {seat, person}) do
-    send(pid, {:reserve, {seat, person}})
-  end
-
-  def status(pid, seat) do
-    send(pid, {:status, seat})
-  end
-
+  #  Start spawn a single SRS process
   def start(name, participants) do
     pid = spawn(SRS, :init, [name, participants])
     pid
   end
 
-  # State Getter
-  def gets(pid) do
-    send(pid, {:get})
-  end
-
+  # Init of state and run
   def init(name, participants) do
+    # Init underlying Paxos protocol
     paxos = Paxos.start(name, participants, self())
 
     state = %{
       name: name,
       paxos: paxos,
       seats: %{
-        seat1: :none,
-        seat2: :none,
-        seat3: :none,
-        seat4: :none,
-        seat5: :none
+        seat1: :free,
+        seat2: :free,
+        seat3: :free,
+        seat4: :free,
+        seat5: :free
       }
     }
 
     run(state)
+  end
+
+  # Reserve a seat for a person
+  def reserve(pid, {seat, person}) do
+    send(pid, {:reserve, {seat, person}})
+  end
+
+  # Get status of a seat
+  def status(pid, seat) do
+    send(pid, {:status, seat})
+  end
+
+  # State Getter
+  def gets(pid) do
+    send(pid, {:get})
   end
 
   # Run function
@@ -46,12 +51,13 @@ defmodule SRS do
           IO.puts("#{state.name} received reserve for #{person}")
           status = Map.get(state.seats, seat)
 
+          # Checking if seat is taken
           case status do
-            status when status != :none ->
+            status when status != :free ->
               IO.puts("#{inspect(seat)} was already booked by #{status}")
               state
 
-            status when status == :none ->
+            status when status == :free ->
               # IO.puts("#{state.name} proposed")
               Paxos.propose(state.paxos, {seat, person})
               Paxos.start_ballot(state.paxos)
@@ -61,6 +67,7 @@ defmodule SRS do
           Process.sleep(300)
           state
 
+        # Return status of seat in Map
         {:status, seat} ->
           stat = Map.get(state.seats, seat)
           IO.puts("#{seat} booked by #{stat}")
@@ -71,6 +78,7 @@ defmodule SRS do
           IO.inspect(state)
           state
 
+        # Book seat if Paxos Consensus is reached
         {:decide, {seat, person}} ->
           # IO.inspect({:decide, {seat, person}})
           state = %{state | seats: Map.replace!(state.seats, seat, person)}
