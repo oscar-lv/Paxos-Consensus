8a131343dc60c51784cec231d5ce2bceb0287405
oscar <ol00130@surrey.ac.uk>
2020-06-04 14:19:24 +0200

    "Changed dist to all dist tests"

--- test_script_dist.exs
+++ test_script_dist.exs
@@ -31,10 +31,10 @@ end
 
 test_suite = [
   #   test case, configuration, number of times to run the case, description
-  {&PaxosTest.run_simple/3, get_local_config.(3), 1, "No failures, no concurrent ballots"},
+  {&PaxosTest.run_simple/3, get_dist_config.(3), 1, "No failures, no concurrent ballots"},
   {&PaxosTest.run_simple_2/3, get_dist_config.(3), 1, "No failures, 2 concurrent ballots"},
   {&PaxosTest.run_simple_many/3, get_dist_config.(5), 1, "No failures, many concurrent ballots"},
-  {&PaxosTest.run_non_leader_crash/3, get_local_config.(3), 1,
+  {&PaxosTest.run_non_leader_crash/3, get_dist_config.(3), 1,
    "One non-leader crashes, no concurrent ballots"},
   {&PaxosTest.run_minority_non_leader_crash/3, get_dist_config.(5), 1,
    "Minority non-leader crashes, no concurrent ballots"},
